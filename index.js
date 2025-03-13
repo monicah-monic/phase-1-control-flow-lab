703e0d0ea9d571c8a8bd5b74b372cc3726ffde03
monicah-monic <monicahwanjiru205@gmail.com>
2025-03-13 15:30:44 +0300

    contro flow

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(sampleRide){
+if(sampleRide<=400){
+  return  'This one is on me!'
+} else if(sampleRide>400 && sampleRide<2000){
+return "That will be twenty bucks.";
+}else if(sampleRide>2000 && sampleRide<2500){
+  return "I will gladly take your thirty bucks.";
+}else{
+  return "No can do.";
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  // return city=== 'NYC'? 'Ok, sounds good.': 'No go.';
+    return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(generous){
   // Write your code here!
+  switch(generous){
+    case 'generous':
+    return "Thank you so much."
+    case 'not as generous':
+      return 'Thank you.'
+      default:
+        return "Bye.";
+  }
 }
